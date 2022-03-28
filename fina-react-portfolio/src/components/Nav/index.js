36ba222823cf0804767e751b0713e191b9664e7c
Tammy Fina <finatammy@gmail.com>
2022-03-27 20:52:07 -0400

    Update index.js
    forced update, having issues with untracked files in github

--- fina-react-portfolio/src/components/Nav/index.js
+++ fina-react-portfolio/src/components/Nav/index.js
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav(props) {
+function Nav(props) {  
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+
   const {
     categories = [],
     setCurrentCategory,
@@ -12,10 +16,6 @@ function Nav(props) {
     console.log(item);
     return item;
   };
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
   return (
     <header data-testid="header" className="flex-row px-1">
