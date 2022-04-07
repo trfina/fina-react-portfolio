2be5d0590de6c9c1237f2f0ad30624368ab8e611
Tammy Fina <finatammy@gmail.com>
2022-04-07 11:36:17 -0400

    contact form structure

--- fina-port/src/App.js
+++ fina-port/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -24,7 +25,8 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <ContactForm></ContactForm>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
--- fina-port/src/components/Contact/index.js
+++ fina-port/src/components/Contact/index.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react';
+
+
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const { name, email, message } = formState;
+
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    
+
+
     return (
         <section>
             <h1>Contact me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} name="name" />    
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={formState.name} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
